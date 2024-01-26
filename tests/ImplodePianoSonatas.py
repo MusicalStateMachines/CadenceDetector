dcc1dc9dc2fc2d5a00603894790dd4ad4593da18
MusicalStateMachines <47847073+MusicalStateMachines@users.noreply.github.com>
2024-01-26 11:19:13 +0200

    Delete tests/ImplodePianoSonatas.py
    removing unrelated files

--- tests/ImplodePianoSonatas.py
+++
@@ -1,7 +0,0 @@
-import music21 as m21
-fileString = r'/Users/matanba/Downloads/Sonate_No._8_Pathetique_3rd_Movement.xml'
-NoteStream = m21.converter.parse(fileString)
-NoteStream.partsToVoices()
-NoteStream.write(fp=r'/Users/matanba/Downloads/Sonate_No._8_Pathetique_3rd_Movement2.xml')
-
-
